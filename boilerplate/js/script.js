c2316fbbefd2aaae9a377926d7f6602adc5dd0c8
rusyamurzin <scretch235@mail.ru>
2018-09-07 17:24:57 +0300

    homework about apples and breads

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,1 +1,9 @@
-console.log('You are at ' + window.location)
+let money = parseInt(prompt('Сколько у вас денег?'))
+let apples = parseInt(prompt('Сколько у вас яблок?')) 
+let breads= parseInt(prompt('Сколько у вас батонов хлеба?')) 
+let priceApple = parseInt(prompt('Сколько стоит 1 яблоко?')) 
+let priceBread = parseInt(prompt('Сколько стоит 1 хлеб?'))
+let sum = money - apples*priceApple - breads*priceBread
+let res = sum > 0
+
+document.body.innerHTML = res 
